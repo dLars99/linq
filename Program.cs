21a18352cac2c10da90e2de51f6e8c5d3deda41d
David Larsen <davidplarsen@gmail.com>
2020-08-27 15:30:48 -0500

    Using custom types

--- Program.cs
+++ Program.cs
@@ -210,6 +210,58 @@ namespace Linq
             Console.WriteLine("\nNumbers in List Until a Perfect Square was Reached");
             Console.WriteLine("--------------------------------------------------");
             notYetSquare.ForEach(num => Console.Write($"{num} "));
+
+            // Build a collection of customers who are millionaires
+
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer() { Name = "Bob Lesman", Balance = 80345.66, Bank = "FTB" },
+                new Customer() { Name = "Joe Landy", Balance = 9284756.21, Bank = "WF" },
+                new Customer() { Name = "Meg Ford", Balance = 487233.01, Bank = "BOA" },
+                new Customer() { Name = "Peg Vale", Balance = 7001449.92, Bank = "BOA" },
+                new Customer() { Name = "Mike Johnson", Balance = 790872.12, Bank = "WF" },
+                new Customer() { Name = "Les Paul", Balance = 8374892.54, Bank = "WF" },
+                new Customer() { Name = "Sid Crosby", Balance = 957436.39, Bank = "FTB" },
+                new Customer() { Name = "Sarah Ng", Balance = 56562389.85, Bank = "FTB" },
+                new Customer() { Name = "Tina Fey", Balance = 1000000.00, Bank = "CITI" },
+                new Customer() { Name = "Sid Brown", Balance = 49582.68, Bank = "CITI" }
+            };
+            List<Customer> millionaires = customers.Where(cust => cust.Balance >= 1000000).ToList();
+
+            Console.WriteLine("\nCustomers Who are Millionaires");
+            Console.WriteLine("------------------------------");
+            millionaires.ForEach(mill => Console.WriteLine(mill.Name));
+
+            /*
+                Given the same customer set, display how many millionaires per bank.
+                Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+                Example Output:
+                WF 2
+                BOA 1
+                FTB 1
+                CITI 1
+            */
+
+            List<Bank> millionairesByBank = (from client in millionaires group client by client.Bank into bankCustomers select new Bank { Name = bankCustomers.Key, clients = bankCustomers.Count() }).ToList();
+
+            Console.WriteLine("\nHow Many Millionaires Per Bank?");
+            Console.WriteLine("-------------------------------");
+            millionairesByBank.ForEach(bank => Console.WriteLine($"{bank.Name} {bank.clients}"));
         }
+
     }
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+
+    public class Bank
+    {
+        public string Name { get; set; }
+        public int clients { get; set; }
+    }
+
 }
