cf806f74faf26f0e83e6bf6ac2ca069794cdb501
David Larsen <davidplarsen@gmail.com>
2020-08-27 13:50:53 -0500

    Complete multiples of 4 or 6

--- Program.cs
+++ Program.cs
@@ -16,9 +16,39 @@ namespace Linq
             where fruit[0] == 'L'
             select fruit;
 
+            Console.WriteLine("Fruits Which Start with 'L'");
+            Console.WriteLine("---------------------------");
             foreach (string l in LFruits)
             {
                 Console.WriteLine($"{l}");
+            }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(num => num % 4 == 0 || num % 6 == 0);
+
+            Console.WriteLine("\nMultiples of 4 or 6");
+            Console.WriteLine("-------------------");
+            foreach (int num in fourSixMultiples)
+            {
+                Console.WriteLine(num);
             }
         }
     }
